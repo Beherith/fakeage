d784c69ae8568e8dbcc122aa77bc9bf455bb8f6a
Tamas Levai <levait@tmit.bme.hu>
2020-09-10 20:30:13 +0200

    Use f-strings
    More about f-strings: https://realpython.com/python-f-strings/

--- fakeage_server.py
+++ fakeage_server.py
@@ -81,7 +81,7 @@ class Game:
             if len(line) == 2:
                 self.questions.append([line[0], unidecode_allcaps_shorten32(line[1])])
         questionsfile.close()
-        print('Loaded', len(self.questions), 'questions')
+        print(f'Loaded {len(self.questions)} questions')
 
     def reset(self):
         self.loadquestions()
@@ -98,19 +98,19 @@ class Game:
         if len(playername) > 32:
             playername = playername[0:32]
         if playername in iter(self.players.values()):
-            print(client, 'tried to log in as an existing playername!', playername)
+            print(f'{client} tried to log in as an existing playername {playername}!')
             return -1
         if playername in self.disconnected_players:
             (score, likecount) = self.disconnected_players.pop(playername)
             self.players[client] = playername
             self.scores[playername] = score
             self.likecount[playername] = likecount
-            print('%s reconnected, score = %d, likecount = %d'%(playername, score, likecount))
+            print(f'{playername} reconnected, score = {score}, likecount = {likecount}')
         else:
             self.players[client] = playername
             self.scores[playername] = 0
             self.likecount[playername] = 0
-            print('%s had joined'%playername)
+            print(f'{playername} had joined')
         return 0
 
     def removeplayer(self, client):
@@ -155,7 +155,9 @@ class Game:
                 'likes': self.likes[player] if player in self.likes else None,
                 'likecount': self.likecount[player] if player in self.likecount else 0,
                 'choice': self.choices[player] if player in self.choices else None})
-        print('%i viewers, %i players, viewinfo: %s' % (len(self.viewers), len(self.players), str(gamestatedict)))
+        print(f'{len(self.viewers)} viewers, '
+              f'{len(self.players)} players, '
+              f'viewinfo: {gamestatedict}')
         return gamestatedict
 
     def updatescoreorder(self):
@@ -166,7 +168,7 @@ class Game:
             for selectorname, choice in self.choices.items():
                 if lie == choice and liername != selectorname:
                     lieselectioncount += 1
-                    print('Lier:', liername, 'with lie', lie, 'got chosen by', selectorname)
+                    print(f'Lier: {liername} with lie {lie} got chosen by {selectorname}')
             if lieselectioncount > 0 and (lie, lieselectioncount) not in self.scoreorder:
                 self.scoreorder.append((lie, lieselectioncount))
         #for likername, like in self.likes.iteritems():  # who likes which lie
@@ -179,46 +181,50 @@ class Game:
             if choice == self.answer:
                 correctcount += 1
         self.scoreorder.append((self.answer, correctcount))  # score truth very last
-        print('game.scoreorder=', self.scoreorder)
+        print(f'game.scoreorder={self.scoreorder}')
         return self.scoreorder
 
     def lie_selection_received(self, client, selectedlie):
         if game.state != 'lieselection':
-            print('%s tried to choose lie %s out of time' % (game.players[client], selectedlie))
+            print(f'{game.players[client]} tried to choose lie {selectedlie} out of time')
             return -1
         player_who_selected_lie = self.players[client]
         if player_who_selected_lie in self.choices:
-            print('Player %s tried to select another lie (%s) despite already having chose (%s)'%(player_who_selected_lie, selectedlie, self.choices[player_who_selected_lie]))
+            print(f'Player {player_who_selected_lie} tried to select another lie '
+                  f'({selectedlie}) despite already having chose '
+                  f'({self.choices[player_who_selected_lie]})')
             return -1
 
         if player_who_selected_lie in self.lies and self.lies[player_who_selected_lie] == selectedlie:
-            print('Player %s tried to select their own lie (%s)'%(player_who_selected_lie, selectedlie))
+            print(f'Player {player_who_selected_lie} tried to select their own lie ({selectedlie})')
             return -1
 
         self.choices[player_who_selected_lie] = selectedlie
 
         if selectedlie == self.answer:
             self.scores[player_who_selected_lie] += 1
-            print('Player %s got the answer (%s) correctly (%s)'%(player_who_selected_lie, self.answer, selectedlie))
+            print(f'Player {player_who_selected_lie} got the answer '
+                  f'({self.answer}) correctly ({selectedlie})')
 
         for liername, lie in self.lies.items():  # who chose which lie
             if lie == selectedlie and liername != player_who_selected_lie:
                 self.scores[liername] += 1
-                print('Lier %s with lie %s got chosen by %s'%(liername, lie, player_who_selected_lie))
+                print(f'Lier {liername} with lie {lie} got chosen by {player_who_selected_lie}')
         self.updatescoreorder()
         return 0
 
     def like_recieved(self, client, likes):
         if self.state != 'lieselection':
-            print('%s tried to like lie %s out of time' % (self.players[client], likes))
+            print(f'{self.players[client]} tried to like lie {likes} out of time')
             return -1
         player_who_liked = self.players[client]
         if player_who_liked in self.likes:
-            print('Player %s tried to like another submission (%s) despite already having chosen (%s)' % (player_who_liked, likes, self.likes[player_who_liked]))
+            print(f'Player {player_who_liked} tried to like another submission ({likes}) '
+                  f'despite already having chosen ({self.likes[player_who_liked]})')
             return -1
 
         if player_who_liked in self.lies and likes == self.lies[player_who_liked]:
-            print('Player %s tried to like their own lie (%s)' % (player_who_liked, likes))
+            print(f'Player {player_who_liked} tried to like their own lie ({likes})')
             return -1
 
         self.likes[player_who_liked] = likes
@@ -226,29 +232,29 @@ class Game:
         for liername, lie in self.lies.items():  # who chose which lie
             if lie == likes and liername != player_who_liked:
                 self.likecount[liername] += 1
-                print('Player:', player_who_liked, ' likes ', lie, ' by', liername)
+                print(f'Player: {player_who_liked} likes {lie} by {liername}')
         return 0
 
     def submitquestion(self, q_and_a):
         try:
             questionsfile = open(self.questionsfilename, 'a')
             question, _, answer = unidecode(q_and_a).rpartition(':')
-            print('Question submitted:', q_and_a)
+            print(f'Question submitted: {q_and_a}')
             questionsfile.write(question + '\t' + unidecode_allcaps_shorten32(answer) + '\n')
         except UnicodeDecodeError:
-            print('Failed to decode unicode string ', q_and_a)
+            print(f'Failed to decode unicode string {q_and_a}')
         except IOError:
-            print('Failed to open file', self.questionsfilename)
+            print(f'Failed to open file {self.questionsfilename}')
         finally:
             questionsfile.close()
 
     def nextquestion(self):
         self.time()
-        if len(self.questions) == 0:
+        if not self.questions:
             self.reset()
         (self.question, self.answer) = self.questions.pop(0)
 
-        print('The current question and answer are:', self.question, self.answer)
+        print(f'The current question and answer are: {self.question} {self.answer}')
         self.choices = {}
         self.likes = {}
         self.lies = {}
@@ -263,9 +269,9 @@ class Game:
 game = Game()  # this is a global variable, we hope that threading wont fuck it up and the Global interpreter lock helps us
 
 
-def unidecode_allcaps_shorten32(s):
-    s = unidecode(s)
-    return s[0:min(len(s), 32)].upper()
+def unidecode_allcaps_shorten32(string):
+    tmp = unidecode(string)
+    return tmp[0:min(len(tmp), 32)].upper()
 
 
 def updategameview(recipients='all'):
@@ -284,7 +290,7 @@ def updategameview(recipients='all'):
 
 def scoring(game):
     print('Scoring called')
-    if len(game.scoreorder) == 0:  # done, advance state
+    if not game.scoreorder:  # done, advance state
         print('Done with scores, advancing automatically to finalscoring')
         game.state = 'finalscoring'
         updategameview()
@@ -351,8 +357,8 @@ def handleTick():
 class WSFakeageServer(WebSocket):
     def handleMessage(self):
         global game
-        print('Message from:', self.client, ' data:', self.data)
-        self.sendMessage("Echo:" + self.data)
+        print(f'Message from: {self.client} data: {self.data}')
+        self.sendMessage(f'Echo: {self.data}')
         if ':' in self.data:
             command, _, parameter = self.data.partition(':')
 
@@ -373,10 +379,10 @@ class WSFakeageServer(WebSocket):
 
             if command == 'lie':
                 if game.state != 'lietome':
-                    print('%s tried to submit lie %s out of time' % (game.players[self], parameter))
+                    print(f'{game.players[self]} tried to submit lie {parameter} out of time')
                 else:
                     if game.players[self] in game.lies:
-                        print('%s tried to lie multiple times!', game.players[self])
+                        print(f'{game.players[self]} tried to lie multiple times!')
                     else:
                         game.lies[game.players[self]] = unidecode_allcaps_shorten32(parameter)
                         updategameview('viewers')
@@ -404,7 +410,7 @@ class WSFakeageServer(WebSocket):
                     scoring(game)
                 else:
                     newstate = game.states[max(0, (game.states.index(game.state)+1)%len(game.states))]
-                    print('Advancing state through viewer: from', game.state, 'to', newstate)
+                    print(f'Advancing state through viewer: from {game.state} to {newstate}')
                     if newstate == 'pregame':
                         game.forcestart = True
                     game.time()
@@ -412,11 +418,11 @@ class WSFakeageServer(WebSocket):
                     updategameview()
 
     def handleConnected(self):
-        print(self.address, 'connected')
+        print(f'{self.address} connected')
         game.clients.append(self)
 
     def handleClose(self):
-        print((self.address, 'disconnected, removing'))
+        print(f'{self.address} disconnected, removing')
         global game
         game.removeplayer(self)
 
@@ -431,28 +437,26 @@ if __name__ == "__main__":
     parser.add_option("--autoadvance", action="store_true", dest="autoadvance", help="Automatically advance game stages")
 
     (options, args) = parser.parse_args()
-    print("Options = ", options)
+    print(f'Options = {options}')
 
-
-    my_ip = '127.0.0.1'
+    my_ip = options.host
     if options.host == '':  # automatically generate ws ip
         my_ip = socket.gethostbyname(socket.gethostname())
-        print('No host ip set, using:', my_ip)
-    else:
-        my_ip = options.host
+        print(f'No host ip set, using: {my_ip}')
 
-    myurl = 'http://%s:%d'%(my_ip, options.httpport)
-    print("Server running at:", myurl)
+    myurl = f'http://{my_ip}:{options.httpport}'
+    print(f'Server running at: {myurl}')
     myqrcode = pyqrcode.create(myurl)
     myqrcode.png('qrcode.png', scale=6)
 
-    # Set the ip in the js file on each launch of the server. This seems pretty hacky, but i couldnt think of anything better
+    # Set the IP in the js file on each launch of the server.
+    # This seems pretty hacky, but i couldnt think of anything better
     websocket_ip_fn = "websocket_ip.js"
     websocket_ip_file = open(websocket_ip_fn)
     websocket_ip_file_text = websocket_ip_file.readlines()
     websocket_ip_file.close()
-    websocket_ip_file_text[1] = '   return "ws://' + my_ip + ':' + str(options.wsport) + '/"\n'
-    print('     return "ws://' + my_ip + ':' + str(options.wsport) + '/"\n')
+    websocket_ip_file_text[1] = '   return "ws://{}:{}/"\n'.format(my_ip, str(options.wsport))
+    print(websocket_ip_file_text[1])
 
     websocket_ip_file = open(websocket_ip_fn, 'w')
     websocket_ip_file.write(''.join(websocket_ip_file_text))
@@ -462,8 +466,6 @@ if __name__ == "__main__":
     game.autoadvance = options.autoadvance
 
     game.loadquestions(options.questions)
-
-    #print "Testing unicode cleanup:", unidecode_allcaps_shorten32(u'Búzafűlé     \tcsicskalangos teszteles')
 
     wsserver = SimpleWebSocketServer(my_ip, options.wsport, WSFakeageServer, selectInterval=0.1)
     wsserver.handleTick = handleTick
